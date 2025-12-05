1b4b5e999bfe680dbcf13045b61c3a96b4746f67
mohnaj70-cmyk <mohnaj70@gmail.com>
2025-12-05 20:51:08 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/autoparts-unified/", 
   server: {
     host: "::",
     port: 8080,
