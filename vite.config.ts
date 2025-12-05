e4e9930f4f95c60b8165cfdd790a313db3e30a91
mohnaj70-cmyk <mohnaj70@gmail.com>
2025-12-05 21:01:21 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/autoparts-unified/", 
+ 
+  base: "/",
+
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
